1a9e9555833ee3b1b2d4e6aa7eb73ed1f5efddf8
Matt Rawding <mattrawding@gmail.com>
2013-04-14 22:00:24 -0400

    Completed 2-card matching game

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -8,29 +8,46 @@
 
 #import "CardGameViewController.h"
 #import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
-@property (strong, nonatomic) Deck *deck;
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *threeMatchMode;
 @end
 
 @implementation CardGameViewController
 
-- (Deck *)deck
+- (CardMatchingGame *)game
 {
-    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
-    return _deck;
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                          usingDeck:[[PlayingCardDeck alloc] init]];
+    return _game;
 }
 
 - (void)setCardButtons:(NSArray *)cardButtons
 {
     _cardButtons = cardButtons;
+    [self updateUI];
+}
+
+- (void)updateUI
+{
     for (UIButton *cardButton in self.cardButtons) {
-        Card *card = [self.deck drawRandomCard];
+        Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
         [cardButton setTitle:card.contents forState:UIControlStateSelected];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
+        cardButton.selected = card.isFaceUp;
+        cardButton.enabled = !card.isUnplayable;
+        cardButton.alpha = (card.isUnplayable ? 0.3 : 1.0);
     }
+    
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
+    self.statusLabel.text = self.game.status;
 }
 
 - (void)setFlipCount:(int)flipCount
@@ -40,8 +57,19 @@
 }
 
 - (IBAction)flipCard:(UIButton *)sender {
-    sender.selected = !sender.isSelected;
+    [self.game flipCardAtIndex:[self.cardButtons indexOfObject:sender]];
     self.flipCount++;
+    self.threeMatchMode.enabled = NO;
+    [self updateUI];
+}
+
+- (IBAction)deal
+{
+    _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                              usingDeck:[[PlayingCardDeck alloc] init]];
+    [self setFlipCount:0];
+    self.threeMatchMode.enabled = YES;
+    [self updateUI];
 }
 
 @end
--- Matchismo/CardMatchingGame.m
+++ Matchismo/CardMatchingGame.m
@@ -10,6 +10,7 @@
 
 @interface CardMatchingGame ()
 @property (readwrite, nonatomic) int score;
+@property (readwrite, nonatomic) NSString *status;
 @property (strong, nonatomic) NSMutableArray *cards; // of Card
 @end
 
@@ -32,6 +33,8 @@
         }
     }
     
+    self.status = @"New game started";
+    
     return self;
 }
 
@@ -42,30 +45,45 @@
 
 #define MATCH_BONUS 4
 #define MISMATCH_PENALTY 2
+#define FLIP_COST 1
 
 - (void)flipCardAtIndex:(NSUInteger)index
 {
     Card *card = [self cardAtIndex:index];
     if (card && !card.isUnplayable) {
-        // loop through all other cards looking for any others that are
-        // face up and still playable
-        for (Card *otherCard in self.cards) {
-            if (otherCard.isFaceUp && otherCard.isUnplayable) {
-                int matchScore = [card match:@[otherCard]];
-                if (matchScore) {
-                    card.unplayable = YES;
-                    otherCard.unplayable = YES;
-                    self.score += matchScore * MATCH_BONUS;
-                } else {
-                    card.faceUp = NO;
-                    otherCard.faceUp = NO;
-                    self.score -= MISMATCH_PENALTY;
+        
+        // only look for a match when we are flipping the card to face up
+        if (!card.isFaceUp) {
+            
+            // loop through all other cards looking for any others that are
+            // face up and still playable
+            BOOL otherCardsFlipped = NO;
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isFaceUp && !otherCard.isUnplayable) {
+                    otherCardsFlipped = YES;
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        card.unplayable = YES;
+                        otherCard.unplayable = YES;
+                        self.score += matchScore * MATCH_BONUS;
+                        self.status = [@[@"Matched", card.contents, @"&", otherCard.contents, @"for", [NSString stringWithFormat:@"%i", matchScore * MATCH_BONUS], @"points"] componentsJoinedByString:@" "];
+                    } else {
+                        otherCard.faceUp = NO;
+                        self.score -= MISMATCH_PENALTY;
+                        self.status = [@[card.contents, @"and", otherCard.contents, @"don't match!", [NSString stringWithFormat:@"%i", MISMATCH_PENALTY], @"point penalty!"] componentsJoinedByString:@" "];
+                    }
+                    break;
                 }
-                break;
             }
+            self.score -= FLIP_COST;
+            if (!otherCardsFlipped){
+                self.status = [@"Flipped up " stringByAppendingString:card.contents];
+            }
+        } else {
+            // we are turning the card face down
+            self.status = [@"Flipped down " stringByAppendingString:card.contents];
         }
         card.faceUp = !card.isFaceUp;
-        
     }
 }
 
